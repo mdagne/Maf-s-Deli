07ab8645b9eb5792eb51ee6b459278736bf5ce69
mierafdagne <mieraf.dagne@seattlecolleges.edu>
2025-11-13 13:38:16 -0800

    Bread and drink enum added

---
+++ src/main/java/com/pluralsight/enums/BreadType.java
@@ -0,0 +1,6 @@
+package com.pluralsight.enums;
+
+public enum BreadType {
+    WHITE, WHEAT, RYE, WRAP;
+}
+
---
+++ src/main/java/com/pluralsight/enums/DrinkSize.java
@@ -0,0 +1,6 @@
+package com.pluralsight.enums;
+
+public enum DrinkSize {
+    SMALL, MEDIUM, LARGE;
+}
+
